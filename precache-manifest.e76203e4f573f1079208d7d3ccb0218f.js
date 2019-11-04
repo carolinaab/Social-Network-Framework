15dbe4ca57376ef9e340d49a3fe84538852ac704
carolinaab <tau_karo@hotmail.com>
2019-11-03 19:36:47 -0600

    Updates

---
+++ precache-manifest.e76203e4f573f1079208d7d3ccb0218f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c01981417fc98a5e06ca2a631d1559e",
+    "url": "/Social-Network-Framework/index.html"
+  },
+  {
+    "revision": "ab75164a7e2e6b4978f0",
+    "url": "/Social-Network-Framework/static/css/main.3ee04d95.chunk.css"
+  },
+  {
+    "revision": "305b67293dddf5207787",
+    "url": "/Social-Network-Framework/static/js/2.50eb9969.chunk.js"
+  },
+  {
+    "revision": "ab75164a7e2e6b4978f0",
+    "url": "/Social-Network-Framework/static/js/main.75f9f36a.chunk.js"
+  },
+  {
+    "revision": "4d9865dcf05564db43ce",
+    "url": "/Social-Network-Framework/static/js/runtime-main.7ed85a3e.js"
+  },
+  {
+    "revision": "d54916909f129591e0344474c8df7a6b",
+    "url": "/Social-Network-Framework/static/media/fondo-registro.d5491690.jpg"
+  },
+  {
+    "revision": "5715f4c7f3917925dee513efcbe05a71",
+    "url": "/Social-Network-Framework/static/media/gmail.5715f4c7.png"
+  },
+  {
+    "revision": "b456a80d1d2c7a493494f75bb8a2df4c",
+    "url": "/Social-Network-Framework/static/media/plan-muro.b456a80d.jpg"
+  }
+]);
